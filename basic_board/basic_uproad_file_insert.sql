d88aca66992901d8a3a7e7fa1e116a083d5d6cea
wooseok718 <dntjr123589@naver.com>
2025-01-14 18:39:50 +0900

    우석 자유게시판 파일

---
+++ basic_board/basic_uproad_file_insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO BASIC_UPLOAD_FILE (basic_origin_file_name, basic_mdf_file_name, basic_file_extension, basic_post_writer) VALUES
+('file1.jpg', 'file1_mdf.jpg', 'jpg', 12),
+('file2.png', 'file2_mdf.png', 'png', 5),
+('file3.pdf', 'file3_mdf.pdf', 'pdf', 18),
+('file4.docx', 'file4_mdf.docx', 'docx', 9),
+('file5.txt', 'file5_mdf.txt', 'txt', 3),
+('file6.zip', 'file6_mdf.zip', 'zip', 21),
+('file7.mp4', 'file7_mdf.mp4', 'mp4', 14),
+('file8.xlsx', 'file8_mdf.xlsx', 'xlsx', 8),
+('file9.pptx', 'file9_mdf.pptx', 'pptx', 11),
+('file10.mp3', 'file10_mdf.mp3', 'mp3', 17);
