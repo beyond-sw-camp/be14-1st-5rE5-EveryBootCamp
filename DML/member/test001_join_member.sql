82c30c73d6d6bdc50d40bb0f3d6107323b528eae
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-01-16 16:12:29 +0900

    [ADD] test001 회원가입 테스트 케이스

---
+++ DML/member/test001_join_member.sql
@@ -0,0 +1,4 @@
+-- 회원 가입
+INSERT INTO MEMBER (member_email, member_pw, member_name, member_no, member_pnum, member_photo, member_cmplt_yn, member_blocklist_yn, member_blocklist_date, member_cardinal_id)
+VALUES
+    ('wooseok@example.com', 'passwoo123', '우셕', '990101-1003584', '010-1234-5671', NULL, 'y', 'n', NULL, 14);
