949d94bc5e884241b1ae8cf11892aae699fc2f33
HMYIEN <bsm0407@naver.com>
2025-01-16 18:31:16 +0900

    TEST052 취업 정보 게시글 검색

---
+++ DML/emp_info_board/test052.sql
@@ -0,0 +1,11 @@
+SELECT
+       emp_id
+     , emp_title
+     , emp_content
+     , emp_date
+     , emp_view
+     , emp_anonym_yn
+     , emp_erase_yn
+     , emp_writer
+  FROM emp_info_board
+ WHERE emp_title LIKE '%프론트엔드%';
