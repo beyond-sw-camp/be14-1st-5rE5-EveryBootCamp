4444717d5cc6b14f55972ffcf8d8c6b2f996f358
wooseok718 <dntjr123589@naver.com>
2025-01-16 19:54:02 +0900

    개인 일정

---
+++ DML/personal_schedule/personal_schedule_(삭제).sql
@@ -0,0 +1,4 @@
+-- 개인 일정 삭제
+
+DELETE FROM personal_schedule
+WHERE psn_schedule_content = '프로젝트 발표 준비';
---
+++ DML/personal_schedule/personal_schedule_(조회).sql
@@ -0,0 +1,12 @@
+-- 개인 일정 조회
+INSERT INTO personal_schedule
+(
+           psn_schedule_content,
+          psn_schedule_start_date,
+          psn_schedule_end_date,
+          psn_schedule_member_id
+)
+VALUES
+(
+         '팀 미팅','2024-01-03,'2024-01-05', 120 
+);
