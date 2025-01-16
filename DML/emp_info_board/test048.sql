5a8897fbd7272c40d456703383f131fdcbe32d67
HMYIEN <bsm0407@naver.com>
2025-01-16 18:58:03 +0900

    TEST048 수정
    JOIN 삭제

--- DML/emp_info_board/test048.sql
+++ DML/emp_info_board/test048.sql
@@ -1,11 +1,9 @@
 SELECT
-    a.emp_id
-     , a.emp_title
-     , a.emp_content
-     , a.emp_date
-     , a.emp_view
-     , a.emp_anonym_yn
-     , b.member_name
- FROM emp_info_board a
- JOIN member b on (a.emp_writer = b.member_id)
-WHERE a. emp_id = 2;
+       emp_id
+     , emp_title
+     , emp_content
+     , emp_date
+     , emp_view
+     , emp_anonym_yn
+  FROM emp_info_board
+ WHERE emp_id = 2;
