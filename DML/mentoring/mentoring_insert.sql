b76c10af45f26745c60653f008fd523af19cc2da
SungMin-hub <sungmin806@gmail.com>
2025-01-16 17:16:18 +0900

    mentoring_insert update

--- DML/mentoring/mentoring_insert.sql
+++ DML/mentoring/mentoring_insert.sql
@@ -1,8 +1,8 @@
 ALTER TABLE mentoring AUTO_INCREMENT = 1;
 
-INSERT INTO MENTORING (mentor_id) VALUES
-(101),
-(102),
-(103),
-(111),
-(112);
+INSERT INTO MENTORING (mentor_id,mtr_accept_yn) VALUES
+(101,'y'),
+(102,'y'),
+(103,'y'),
+(111,'y'),
+(112,'y');
--- init_settings.sql
+++ init_settings.sql
@@ -312,13 +312,13 @@ VALUES
 
 -- ------------------- MENTORING -------------------------
 ALTER TABLE mentoring AUTO_INCREMENT = 1;
-INSERT INTO MENTORING (mentor_id)
+INSERT INTO MENTORING (mentor_id,mtr_accept_yn)
 VALUES
-    (101),
-    (102),
-    (103),
-    (111),
-    (112);
+    (101,'y'),
+    (102,'y'),
+    (103,'y'),
+    (111,'y'),
+    (112,'y');
 
 
 -- ------------------- MENTEE_FROM_MENTORING -------------------------
