f9b8e70f848fa90ab3e9e97d4ed30b2e6155d817
HMYIEN <bsm0407@naver.com>
2025-01-16 18:25:50 +0900

    TEST49 취업 정보 게시글 작성

---
+++ DML/emp_info_board/test049.sql
@@ -0,0 +1,10 @@
+INSERT
+INTO emp_info_board
+(
+    emp_title, emp_content, emp_date, emp_view, emp_anonym_yn, emp_writer
+)
+VALUES
+(
+  '백엔드 개발자 채용 공고', '백엔드 시스템을 설계하고 유지 보수할 개발자를 모집합니다. 주요 기술 스택: Node.js, Spring Framework. 관련 경력 2년 이상 우대.'
+, '2025-01-15', 0, 'y', 113
+);
