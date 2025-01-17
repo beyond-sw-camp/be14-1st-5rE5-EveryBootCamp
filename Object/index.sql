c0f3d4ff6687a399b01fa4854395154df1647f2d
50071 <rkd50071@gmail.com>
2025-01-17 18:41:35 +0900

    # GIF 폴더 정리 # Object > index 생성 시 or replace 옵션 추가

--- Object/index.sql
+++ Object/index.sql
@@ -1,8 +1,7 @@
-CREATE INDEX idx_mem_name ON MEMBER(member_name);
-
-CREATE INDEX idx_basic_title ON BASIC_BOARD(basic_title);
-CREATE INDEX idx_emp_title ON EMP_INFO_BOARD(emp_title);
-CREATE INDEX idx_study_title ON STUDY_BOARD(study_title);
+CREATE OR REPLACE INDEX idx_mem_name ON MEMBER(member_name);
+CREATE OR REPLACE INDEX idx_basic_title ON BASIC_BOARD(basic_title);
+CREATE OR REPLACE INDEX idx_emp_title ON EMP_INFO_BOARD(emp_title);
+CREATE OR REPLACE INDEX idx_study_title ON STUDY_BOARD(study_title);
 
 SHOW INDEX FROM MEMBER;
 SHOW INDEX FROM BASIC_BOARD;
