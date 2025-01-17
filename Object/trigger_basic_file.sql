9eba46bb4ac1964f5f2548248ac6eeb96e2d4f77
50071 <rkd50071@gmail.com>
2025-01-17 20:08:03 +0900

    #1 trigger_basic_file.sql에 테스트용 insert, select 쿼리 추가 #2 trigger_basic_file.gif

--- Object/trigger_basic_file.sql
+++ Object/trigger_basic_file.sql
@@ -11,3 +11,16 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+INSERT
+  INTO basic_upload_file
+(
+ basic_origin_file_name, basic_mdf_file_name, basic_file_extension, basic_post_id
+)
+VALUES
+(
+ '오레오', CONCAT('오레오', '_', DATE_FORMAT(NOW(), '%Y%m%d_%H%i%S')), 'png', 11
+);
+
+-- 데이터 조회
+SELECT * FROM basic_upload_file ORDER BY basic_file_id DESC;
