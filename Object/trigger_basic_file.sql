a1dd097b7fcd3b5d432b22b14ce0936bf415e473
SungMin-hub <sungmin806@gmail.com>
2025-01-17 16:22:23 +0900

    [UPDATE] trigger_basic_file

--- Object/trigger_basic_file.sql
+++ Object/trigger_basic_file.sql
@@ -7,7 +7,7 @@ BEGIN
     -- BASIC_BOARD 테이블의 basic_erase_yn 값을 Y로 업데이트
     UPDATE BASIC_BOARD
     SET basic_erase_yn = 'Y'
-    WHERE basic_id = NEW.basic_post_id; -- BASIC_UPLOAD_FILE 테이블의 board_id와 매칭
+    WHERE basic_id = NEW.basic_post_id;
 END$$
 
 DELIMITER ;
